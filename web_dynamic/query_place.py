4e65b02a8d642f42616735845dad2b16bc6d0d85
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-08-12 04:02:38 +0100

    remove unnecessary file

--- web_dynamic/query_place.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-from models import storage
-from models.place import Place
-
-places = storage.all(Place).values()
-print(f"Total places in database: {len(list(places))}")
-for place in places:
-    print(f"Place: {place.id}, {place.name}")
